cb04339440c010a10d512ee334cb06d32f81fb71
Jason Wuerch <wuerchjason@gmail.com>
2016-04-01 10:08:37 -0700

    add functionality for answering individual questions with author/content

---
+++ app/components/answer-add.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  askAnswerForm: false,
+  actions: {
+    showAskAnswerForm() {
+      this.set('askAnswerForm', true);
+    },
+    saveAnswer() {
+      var params = {
+        author: this.get('author') ? this.get('author') : "",
+        content: this.get('content') ? this.get('content') : "",
+        question: this.get('question') ? this.get('question') : "",
+      };
+      this.set('askAnswerForm', false);
+      this.sendAction('saveAnswer', params)
+    };
+  }
+});
