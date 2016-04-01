36bc20619f6bcf8ac50bfe9791a9a60ede3055a8
Jason Wuerch <wuerchjason@gmail.com>
2016-04-01 09:49:02 -0700

    add functionality for updating individual questions

--- app/components/question-update.js
+++ app/components/question-update.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    showUpdateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    updateQuestion(question) {
+      var params = {
+        title: this.get('title') ? this.get('title') : "",
+        author: this.get('author') ? this.get('author') : "",
+        note: this.get('note') ? this.get('note') : "",
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('updateQuestion', question, params);
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -10,6 +10,14 @@ export default Ember.Route.extend({
     deleteQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
+    },
+    updateQuestion(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key, params[key]);
+        }
+      });
+      question.save();
     }
   }
 });
