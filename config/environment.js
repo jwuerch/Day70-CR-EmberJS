0465f1ea819543139d33f96c8e35ee7c7fdc5530
Jason Wuerch <wuerchjason@gmail.com>
2016-04-01 08:23:45 -0700

    add firebase, bootstrap and font awesome packages

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'day65-cr-message-board',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://cr-message-board.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
