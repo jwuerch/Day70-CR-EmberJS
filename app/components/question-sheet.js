5e6394824b96cb9573aacdee4a5ad7974e69e9d9
Jason Wuerch <wuerchjason@gmail.com>
2016-04-08 14:59:58 -0700

    add computed property that sorts questions by number of upvotes

--- app/components/question-sheet.js
+++ app/components/question-sheet.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  sortBy: ['rank:desc'],
+  sortedQuestions: Ember.computed.sort('questions', 'sortBy'),
   actions: {
     upVote(question) {
       this.sendAction('upVote', question);
