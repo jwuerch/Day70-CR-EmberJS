896b17e4a15a4dd3646ee9f52437f753ea2ffe52
Jason Wuerch <wuerchjason@gmail.com>
2016-04-08 10:20:39 -0700

    add favorite star on question page if user is logged-in

---
+++ tests/unit/services/add-favorite-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:add-favorite', 'Unit | Service | add favorite', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let service = this.subject();
+  assert.ok(service);
+});
