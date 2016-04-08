7d95d9cbec62c737f74d324dd71021e141b2b8aa
Jason Wuerch <wuerchjason@gmail.com>
2016-04-08 09:58:55 -0700

    session still is signed-in when user refreshes page

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel() {
-    
+    this.get('session').fetch().catch((error) => {
+      console.log(error);
+    });
   },
 
   actions: {
